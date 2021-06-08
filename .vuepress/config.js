c85e3af2f37527a2432ca4b33a9dc442a27c5cd0
shunhuawong <shunhuawong@live.com>
2021-06-09 01:00:07 +0800

    美化操作

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,20 +3,31 @@ module.exports = {
   "description": "小狗窝",
   "dest": "public",
   "head": [
-    [
-      "link",
-      {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
-    ],
-    [
+    ["link", { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" }],
+    ["script", { src: "scripts/demo.js" }],   
+   [   
       "meta",
       {
         "name": "viewport",
         "content": "width=device-width,initial-scale=1,user-scalable=no"
       }
-    ]
+    ],
+  // 网页标签栏图标
+  ['link', { rel: 'icon', href: '/vuepress/favicon.ico' }],
+  // 移动栏优化
+  ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+  // 引入jquery
+  ["script", {
+    "language": "javascript",
+    "type": "text/javascript",
+    "src": "https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js"
+  }],
+  // 引入鼠标点击脚本
+  ["script", {
+    "language": "javascript",
+    "type": "text/javascript",
+    "src": "/js/MouseClickEffect.js"
+  }]
   ],
   "theme": "reco",
   "themeConfig": {
